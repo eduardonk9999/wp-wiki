fdefd948839a40c743cbf0573abd9fa6fc468324
eduardonk9999 <thiago.novak7@gmail.com>
2021-12-22 11:24:10 -0300

    Add, ajuste readme

--- wp-content/themes/rest-inicio/index.php
+++ wp-content/themes/rest-inicio/index.php
@@ -5,7 +5,7 @@
 		<title>Rest</title>
 
 		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 	</head>
 
 	<body>
@@ -19,7 +19,7 @@
 				</ul>
 			</nav>
 
-			<h1><img src="img/rest.png" alt="Rest"></h1>
+			<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
 
 			<p>Rua Marechal 29 – Copacabana – Rj</p>
 			<p class="telefone">2422-9201</p>
