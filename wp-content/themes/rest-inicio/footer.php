1864c0112bb48243168b424cc4675bffd870622f
eduardonk9999 <thiago.novak7@gmail.com>
2021-12-23 05:14:54 -0300

    Add Modelo de PAG

---
+++ wp-content/themes/rest-inicio/footer.php
@@ -0,0 +1,14 @@
+<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="index.html">Menu</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>
